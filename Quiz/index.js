3449b759d4aa64c2398d4905acaf49924342f033
Paola Velasquez <115559752+PaolaVelasquez2611@users.noreply.github.com>
2023-02-07 08:09:09 -0500

    Add files via upload

---
+++ Quiz/index.js
@@ -0,0 +1,23 @@
+
+for (let index = 0; index < 20 ; index++) {
+    const numbers = Array[index];
+    console.log (index);
+
+}
+    function printEnt (num) {
+        i = 1
+        while(i<=num){
+        console.log(i)
+        if(i%3==0 && i%5==0){
+        console.log("by both")
+        }else if(i%3==0){
+        console.log("by 3")
+        }else if(i%5==0){
+        console.log("by 5")
+        }
+        i++
+        }}
+
+        printEnt (19)
+
+        //si funcionaaa
